d1cbdd632d58e69fcc0362f1b956e2e9b987b2e3
bas <bas@ichigendo.nl>
2024-05-17 23:52:02 +0200

    Added feature section and made a start on the parallax 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
-const { src, dest, watch, series} = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
-    return src('assets/css/screen.scss')
-        .pipe(sass())
-        .pipe(purgecss({
-            content: ['**/*.hbs']
-        }))
-        .pipe(dest('assets/built'))
+  return src('assets/css/screen.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(purgecss({
+      content: ['**/*.hbs'],
+      safelist: {
+        standard: [
+            'kg-width-wide',
+            'kg-width-full'
+        ]
+    }
+    }))
+    .on('error', (err) => {
+      console.error('Error in purgecss task', err.toString());
+    })
+    .pipe(dest('assets/built'))
 }
 
 // here we watch an array of files
@@ -16,7 +25,4 @@ function watchTask() {
   return watch(['assets/css/**/*.scss', '**/*.hbs'], buildStyles);
 }
 
-exports.default = series(
-  buildStyles,
-  watchTask
-);
+exports.default = series(buildStyles, watchTask);
