3bb10a28bbfd096ae6316aa7b057e9c3f345bb03
bas <bas@ichigendo.nl>
2024-05-17 11:42:46 +0200

    added gulp-purger

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 const { src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
     return src('assets/css/screen.scss')
         .pipe(sass())
+        .pipe(purgecss({
+            content: ['**/*.hbs']
+        }))
         .pipe(dest('assets/built'))
 }
 
 // here we watch an array of files
 function watchTask() {
-  return watch(['assets/css/**/*.scss'], buildStyles);
+  return watch(['assets/css/**/*.scss', '**/*.hbs'], buildStyles);
 }
 
 exports.default = series(
