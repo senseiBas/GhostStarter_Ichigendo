1d7088ed0761b60d5868995e1f69093f5a8cc950
bas <bas@ichigendo.nl>
2024-05-26 22:09:02 +0200

    added the package.json custom variables, and started fixing the post content

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,14 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
 function buildStyles() {
   return src('assets/css/screen.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(purgecss({
-      content: ['**/*.hbs'],
-      safelist: {
-        standard: [
-            'kg-width-wide',
-            'kg-width-full'
-        ]
-    }
+      content: ['**/*.hbs'], // Exclude the file to be safelisted
     }))
     .on('error', (err) => {
       console.error('Error in purgecss task', err.toString());
@@ -20,9 +16,17 @@ function buildStyles() {
     .pipe(dest('assets/built'))
 }
 
-// here we watch an array of files
-function watchTask() {
-  return watch(['assets/css/**/*.scss', '**/*.hbs'], buildStyles);
+function buildScripts() {
+  return src('assets/js/index.js')
+    .pipe(uglify())
+    .pipe(rename('index.min.js'))
+    .pipe(dest('assets/built'));
 }
 
-exports.default = series(buildStyles, watchTask);
+// here we watch an array of files
+function watchTask() {
+  watch(['assets/css/**/*.scss', '**/*.hbs'], buildStyles);
+  watch('assets/js/**/*.js', buildScripts);
+}
+
+exports.default = series(buildStyles, buildScripts, watchTask);
---
+++ todo.md
@@ -0,0 +1,41 @@
+<!-- Posts -->
+<!-- ------------- -->
+
+<!-- fix block quote -->
+center content like basic ghost
+Gulp to not kill the "base stuff"
+fix images: smaller - center
+fix big imaghes: parallax becasue cool
+center iframe
+fix buttons
+fix gallery
+fix bookmark
+email cta
+callouts
+signup form
+video
+product
+youtube
+twitter
+
+<!-- Programming post pages -->
+<!-- ------------- -->
+
+read more sections
+fix comment sections
+
+
+<!-- POSTS & PAGES -->
+<!-- ------------- -->
+
+fix tag pages
+fix pages (headers uit enzo)
+
+
+<!-- HOME SCREEN -->
+<!-- ------------- -->
+
+remove todos
+fix faq section: if faq tag
+fix read more
+fix footer
